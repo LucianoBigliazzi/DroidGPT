8440b91a6b899ffa80980af2f078a6d6c2faed04
LucianoBigliazzi <luciano.bigliazzi@mail.polimi.it>
2023-09-12 18:58:35 +0200

    Fixed TimeFormats class

--- app/src/main/java/com/droidgpt/model/TimeFormats.java
+++ app/src/main/java/com/droidgpt/model/TimeFormats.java
@@ -6,5 +6,5 @@ public class TimeFormats {
 
     public static final DateTimeFormatter TIME       = DateTimeFormatter.ofPattern("HH:mm");
     public static final DateTimeFormatter DATE       = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    public static final DateTimeFormatter DATE_TIME  =  DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    public static final DateTimeFormatter DATE_TIME  = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 }
